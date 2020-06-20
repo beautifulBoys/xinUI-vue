f76af42e44e0126aa1a6d3fab2063979e22a6680
lixin_boy <1298947916@qq.com>
2020-06-20 17:57:29 +0800

    include docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: 'XinUI 前端组件库',
+  description: '一个基于 Vue 框架的轻量级 UI 组件库',
+  themeConfig: {
+    logo: '/assets/images/logo.png',
+    nav: [
+      {
+        text: '快速开始',
+        link: '/start/'
+      },
+      {
+        text: 'XinUI',
+        link: '/ui/'
+      },
+      {
+        text: 'XinSkill',
+        link: '/skill/'
+      },
+      {
+        text: 'github',
+        link: 'https://github.com/beautifulBoys/xinUI-vue'
+      }
+    ],
+    sidebar: {
+      '/ui/': [
+
+      ],
+      '/skill/': [
+
+      ]
+    }
+  }
+}
