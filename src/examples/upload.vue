80aa61c2b01937723d5ada38663c0d25126ec52b
lixin_boy <1298947916@qq.com>
2020-07-05 10:47:40 +0800

    upload

---
+++ src/examples/upload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="example-page example-viewer">
+    <div class="line">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/skill/upload/main.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    :class="['xin-upload']"
+  >
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'xinUpload',
+  components: {
+  },
+  props: {
+    value: {
+      type: [Number, String],
+      default: ''
+    },
+    list: {
+      type: Array,
+      default: () => []
+    },
+    color: { // blue
+      type: String,
+      default: ''
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    },
+    itemValue: {
+      type: String,
+      default: 'name'
+    },
+    itemLabel: {
+      type: String,
+      default: 'title'
+    },
+    closable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      tabValue: this.value
+    }
+  },
+  created () {
+
+  },
+  watch: {
+    value (n) {
+      this.tabValue = n
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    tabEvent (item) {
+      this.tabValue = item[this.itemValue]
+      this.$emit('tab-change', this.tabValue)
+    },
+    getValue (item) {
+      return this.itemValue ? item[this.itemValue] : item
+    },
+  }
+}
+</script>
