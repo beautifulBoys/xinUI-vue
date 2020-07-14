665042148a30dd2220279d3211e83127eff04cc1
lixin_boy <1298947916@qq.com>
2020-07-14 10:40:51 +0800

    slide

--- src/components/slide/main.vue
+++ src/components/slide/main.vue
@@ -183,6 +183,18 @@ export default {
       this.mouse = ''
       this.emit()
     },
+    generateUUID () {
+      var d = new Date().getTime()
+      if (window.performance && typeof window.performance.now === 'function') {
+        d += performance.now() //use high-precision timer if available
+      }
+      let uuid = 'xxxxyxyxxyxyxyxyxxxyxyxyy'.replace(/[xy]/g, c => {
+        let r = (d + Math.random() * 16) % 16 | 0
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+      })
+      return uuid
+  },
     emit () {
       let val
       if (this.range) {
